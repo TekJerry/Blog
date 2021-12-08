890215428d850815df37853d741fed650d4c3eed
jerrytek <jerrycodingtek@gmail.com>
2021-12-08 13:42:35 -0600

    edited my new header

--- the-anglers-guide/src/components/Header.js
+++ the-anglers-guide/src/components/Header.js
@@ -6,41 +6,7 @@ import "./Header.css"
 export default function Header() {
   return (
     <div className="headero">
-      <Carousel>
-  <Carousel.Item interval={6000}>
-    <img
-      className="d-block-w-100"
-      src="https://i.imgur.com/u7iegZo.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={3000}>
-    <img
-      className="d-block-w-100"
-      src="https://i.imgur.com/Jo1ouol.jpg"
-      alt="Second slide"
-    />
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block-w-100"
-      src="https://i.imgur.com/TiY3SzV.jpg"
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+  <div><h1>The Anglers Guide</h1></div>
     </div>
   )
 }
